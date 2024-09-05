a54b337d3fe1b8a793398ede3a437decf8b622a1
prolay7 <prolay7@hotmail.com>
2024-09-05 12:30:42 +0530

    cron.py

---
+++ cron.py
@@ -0,0 +1,10 @@
+# Import the function from module1
+from dbread import connect_and_read
+#from ocread import connect_and_read_oc
+#from ocrai import connect_and_read_ocai
+
+
+# Call the function
+connect_and_read()
+#connect_and_read_oc()
+#connect_and_read_ocai()
